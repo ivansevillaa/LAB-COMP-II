1e4e18a2d4e9542aa04a78819ed7816240deca9f
Ivan Sevilla <ivansevillaa@gmail.com>
2019-08-08 22:18:55 -0300

    Trabajo práctico N°1

---
+++ trabajo-practico-1/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sn = new Scanner(System.in);
+
+        String nombre;
+        int edad;
+
+        System.out.println("Ingrese su nobre");
+        nombre = sn.nextLine();
+
+        do {
+            System.out.println("Ingrese su edad ");
+            edad = sn.nextInt();
+            if(edad != 0) {
+                if(edad > 17) System.out.println(nombre + " es mayor de edad");
+                else System.out.println(nombre + " es menor de edad");
+            }
+        } while(edad != 0);
+
+        System.out.println("fin.");
+    }
+}
