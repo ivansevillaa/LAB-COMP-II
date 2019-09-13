656ef81441848f8d3ee9b1cc6a1c986ea51b7105
Ivan Sevilla <ivansevillaa@gmail.com>
2019-09-13 08:46:23 -0300

    tp 6

--- src/trabajoPractico6/Main.java
+++ src/trabajoPractico6/Main.java
@@ -1,6 +1,27 @@
 package trabajoPractico6;
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+
+        Scanner sn = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de contraseñas que desea generar");
+        int quantity = sn.nextInt();
+        System.out.println("Ingrese la longitud que desea que tengan sus contraseñas");
+        int length = sn.nextInt();
+
+        PasswordGenerator passwordgenerator = new PasswordGenerator(quantity, length);
+
+        passwordgenerator.makePassword();
+        for (int i = 0; i < passwordgenerator.getPasswords().length; i++) {
+            System.out.println("Contraseña: " + passwordgenerator.getPasswords()[i] +
+                    " Longitud: " + passwordgenerator.getPasswordLength() +
+                    " Segura: " + passwordgenerator.isSecure(passwordgenerator.getPasswords()[i]));
+        }
+
+
     }
 }
--- src/trabajoPractico6/PasswordGenerator.java
+++ src/trabajoPractico6/PasswordGenerator.java
@@ -1,6 +1,54 @@
 package trabajoPractico6;
 
 public class PasswordGenerator {
+    private String passwords[];
+    private int passwordLength;
+    private char[] chars = { '0','1','2','3','4','5','6','7','8','9',
+            'a','b','c','d','e','f','g','h','i','j',
+            'k','l','m','n','o','p','q','r','s','t',
+            'u','v','w','x','y','z','A','B','C','D',
+            'E','F','G','H','I','J','K','L','M','N',
+            'O','P','Q','R','S','T','U','V','W','X',
+            'Y','Z' };
 
+    public PasswordGenerator (int passwordsQuantity, int passwordLength) {
+        this.passwords = new String [passwordsQuantity];
+        this.passwordLength = passwordLength;
+    }
 
+    public String[] getPasswords() {
+        return passwords;
+    }
+
+    public int getPasswordLength() {
+        return this.passwordLength;
+    }
+
+    public void makePassword() {
+        for (int i = 0; i < this.passwords.length; i++) {
+            this.passwords[i] = "";
+            for (int j = 1; j <= this.passwordLength; j++) {
+                this.passwords[i] += chars[(int)(Math.random()*61)];
+            }
+        }
+    }
+
+    public boolean isSecure(String password) {
+        int uppercases = 0, lowercases = 0, numbers = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isUpperCase(password.charAt(i))) {
+                uppercases++;
+            } else if ((Character.isLowerCase(password.charAt(i)))) {
+                 lowercases++;
+            } else {
+                numbers++;
+            }
+        }
+
+        if (numbers > 5 && uppercases > 2 && lowercases > 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
